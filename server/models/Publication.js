01fd9fa8d8330bfb4e60cc599473d6f0fdb90585
Med Aziz Ben Smida <73040491+mouhamedazizbensmida@users.noreply.github.com>
2024-01-12 22:10:58 +0100

    Publication.js

--- server/models/Publication.js
+++ server/models/Publication.js
@@ -1,28 +1,26 @@
 import mongoose from "mongoose";
 
-
-const PublicationSchema =new  mongoose.Schema(
-        {  
-            title: {
-              type: String,
-            },
-            datepublication: {
-                type: Date, default: Date.now 
-            },
-            contenu: {
-              type: String,
-            }, 
-
-           },	
+const PublicationSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+    },
+    datepublication: {
+      type: Date,
+      default: Date.now,
+    },
+    contenu: {
+      type: String,
+    },
+},
+  {
+    toJSON: { virtuals: true }, // Enable virtuals to be included when converting to JSON
+  }
 );
-// Create a virtual property named 'resume'
-PublicationSchema.virtual('resume').get(function () {
-    if (this.contenu) {
-      return this.contenu.slice(10);
-    }
-    return '';
-  });
 
-  
-const Publication = mongoose.model("Publication", PublicationSchema)
+// Define the virtual 'fullName' using the 'get' function
+PublicationSchema.virtual('resume').get(function () {
+  return this.contenu.slice(0,5)
+});
+const Publication = mongoose.model("Publication", PublicationSchema);
 export default Publication;
